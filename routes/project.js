9c10aaffcff87b363cc9a4623286f344eba2ed1c
hunter <gallanthunter@163.com>
2018-03-08 22:53:33 +0800

    修改路由get方法

--- routes/project.js
+++ routes/project.js
@@ -1,20 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var project = require('../models/project');
-var operation = require('../models/operation');
+var projectModel = require('../models/project');
 
-
+// create project
 router.post('/project', function (req, res) {
-    // res.send('POST request to the homepage');
-    operation.createProject(req.body.json(project));
+    projectModel.project.create(req.body, (err, project) = > {
+        if(err) {
+            console.log(err);
+            res.json(err)
+        }
+        else {
+            console.log('save success');
+    res.json(project);
+}
+})
 });
 
 router.get('/project', function (req, res) {
-
+    projectModel.Project.find({})
+        .sort({update_at: -1})
+        .then(projects = > {
+        console.log(projects);
+    console.log(Date.now() + 'This is get message!')
+    res.json(projects)
+})
+.
+    catch(err = > {
+        console.log(err);
+    res.json(err)
+})
 });
 
 router.get('/project/:operator', function (req, res) {
-
+    console.log("********")
 });
 
 router.get('/project/:region', function (req, res) {
@@ -29,5 +47,62 @@ router.get('/project/:scenario', function (req, res) {
 
 });
 
-// POST method route
+// // 通过ObjectId查询单个电影
+// router.get('/movie/:id', (req, res) => {
+//     Movie.findById(req.params.id)
+//     .then(movie => {
+//     res.json(movie)
+// })
+// .catch(err => {
+//     res.json(err)
+// })
+// })
+// // 添加一部电影
+// router.post('/movie', (req, res) => {
+//     //使用Movie model上的create方法储存数据
+//     Movie.create(req.body, (err, movie) => {
+//     if (err) {
+//         res.json(err)
+//     } else {
+//         res.json(movie)
+// }
+// })
+// //使用实例的save方法存储数据
+// // let movie = new Movie({
+// //   title : req.body.title,
+// //   year : req.body.year,
+// //   poster : req.body.poster,
+// //   introduction : req.body.introduction
+// // })
+// // movie.save( (err,movie) => {
+// //   if (err) {
+// //     res.json(err)
+// //   } else {
+// //     res.json(movie)
+// //   }
+// // })
+// })
+// //更新一部电影
+// router.put('/movie/:id',(req,res) => {
+//     Movie.findOneAndUpdate({ _id : req.params.id}
+//     ,{ $set : { title: req.body.title,
+//             rating : req.body.rating,
+//             poster : req.body.poster,
+//             introduction : req.body.introduction }
+//     },{
+//         new : true
+//     })
+//     .then(movie => res.json(movie))
+// .catch(err => res.json(err))
+// })
+// //删除一部电影
+// router.delete('/movie/:id',(req,res) => {
+//     Movie.findOneAndRemove({
+//     _id : req.params.id
+// })
+//     .then(movie => res.send(`${movie.title}删除成功`))
+// .catch(err => res.json(err))
+// })
 
+
+module.exports = router;
