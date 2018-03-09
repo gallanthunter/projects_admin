02f31a3f2db48072e34ad019cdc4d205b66ee5c0
hunter <gallanthunter@163.com>
2018-03-09 21:38:35 +0800

    删除无用数据表

--- models/operator.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema;
-
-var operatorSchema = new Schema({
-    name: {type: String, required: true}
-});
-operatorSchema.index({name: 1});
-
-module.exports = mongoose.model('Operator', operatorSchema);
